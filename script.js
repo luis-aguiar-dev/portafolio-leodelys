537dd9d4ee5e3ab84cb78a1c5a0c5801a6fe4a9e
luis-aguiar-dev <luis.aguiar.25@gmail.com>
2024-01-15 15:26:14 -0400

    Actualizacion 15-01-24

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // Manejo de la galería y el modal
     let fotos = document.querySelectorAll('.gallery-photo img');
     let imagenes = Array.from(fotos).map(img => img.src);
-    let indiceActual = 0;
     let modal = document.getElementById("modalGaleria");
     let imagenModal = document.getElementById("imagenModal");
     let cerrarModal = document.getElementsByClassName("modal-close")[0];
@@ -11,20 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
     function mostrarImagen(indice) {
         if (indice >= 0 && indice < imagenes.length) {
             imagenModal.src = imagenes[indice];
-            indiceActual = indice;
-        }
-    }
-
-    // Navegación de imágenes
-    function siguienteImagen() {
-        if (indiceActual < imagenes.length - 1) {
-            mostrarImagen(indiceActual + 1);
-        }
-    }
-
-    function anteriorImagen() {
-        if (indiceActual > 0) {
-            mostrarImagen(indiceActual - 1);
         }
     }
 
@@ -46,16 +31,4 @@ document.addEventListener("DOMContentLoaded", function() {
             modal.style.display = "none";
         }
     };
-
-    // Crear y añadir botones de navegación en el modal
-    let modalSiguiente = document.createElement("button");
-    modalSiguiente.innerHTML = "<i class='fas fa-arrow-right'></i>"; // Ícono de flecha derecha
-    modalSiguiente.onclick = siguienteImagen;
-
-    let modalAnterior = document.createElement("button");
-    modalAnterior.innerHTML = "<i class='fas fa-arrow-left'></i>"; // Ícono de flecha izquierda
-    modalAnterior.onclick = anteriorImagen;
-
-    modal.appendChild(modalAnterior);
-    modal.appendChild(modalSiguiente);
 });
